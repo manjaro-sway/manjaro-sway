6b1637f3769e5af48f6125fb1c3771cc0ef94eec
Jonas Strassel <info@jonas-strassel.de>
2021-01-21 14:24:07 +0100

    fix(sway): add themes for kitty and align with termite theme

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/terminal.sh
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/terminal.sh
@@ -7,12 +7,12 @@ GL_VERSION=$(glxinfo | awk '/OpenGL version/ {print $4}')
 
 CMD=""
 if [[ -x "$(command -v kitty)" && $(version $GL_VERSION) -ge $(version "3.3") ]]; then
-    CMD="kitty"
+    CMD="kitty --config=\$theme/kitty.conf"
     [ -n "$1" ] && [ "$1" == "float" ] && CMD="$CMD --class=floating_shell"
     [ -n "$1" ] && [ "$1" == "float_portrait" ] && CMD="$CMD --class=floating_shell_portrait"
     [ -n "$1" ] && CMD="$CMD ${2:-$1}"
 else
-    CMD="termite --config=\$theme/termite"
+    CMD="termite --config=\$theme/termite.conf"
     [ -n "$1" ] && [ "$1" == "float" ] && CMD="$CMD --name=floating_shell"
     [ -n "$1" ] && [ "$1" == "float_portrait" ] && CMD="$CMD --name=floating_shell_portrait"
     [ -n "$1" ] && CMD="$CMD --exec ${2:-$1}"
