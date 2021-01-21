cbd2eaf9fb7e2fd95164f85839eb4776bf7f4238
Jonas Strassel <info@jonas-strassel.de>
2021-01-21 13:10:33 +0100

    feat: add a fallback to termite when kitty doesn't work (#2)
    * feat: add a fallback to termite when kitty doesn't work
    
    * fix(sway): use class for floating kitty

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/terminal.sh
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/terminal.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-swaymsg exec "\$term --exec $1"
+function version { 
+    echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; 
+}
+
+GL_VERSION=$(glxinfo | awk '/OpenGL version/ {print $4}')
+
+CMD=""
+if [[ -x "$(command -v kitty)" && $(version $GL_VERSION) -ge $(version "3.3") ]]; then
+    CMD="kitty"
+    [ -n "$1" ] && [ "$1" == "float" ] && CMD="$CMD --class=floating_shell"
+    [ -n "$1" ] && [ "$1" == "float_portrait" ] && CMD="$CMD --class=floating_shell_portrait"
+    [ -n "$1" ] && CMD="$CMD ${2:-$1}"
+else
+    CMD="termite --config=\$theme/termite"
+    [ -n "$1" ] && [ "$1" == "float" ] && CMD="$CMD --name=floating_shell"
+    [ -n "$1" ] && [ "$1" == "float_portrait" ] && CMD="$CMD --name=floating_shell_portrait"
+    [ -n "$1" ] && CMD="$CMD --exec ${2:-$1}"
+fi
+
+echo $CMD 
+swaymsg exec "$CMD"
