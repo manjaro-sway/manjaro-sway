22bb885fc0165a8367bae5069e1e7c5bfc01666c
Jonas Strassel <info@jonas-strassel.de>
2021-07-31 18:26:18 +0200

    feat: toggle sunset mode (#36)
    * feat: toggle sunset mode
    
    * feat: display wlsunset waybar module if installed
    
    * feat: fallback to configured values
    
    * fix: safer fallbacks
    
    * feat: make config file optional...
    
    ... by providing the defaults inside the script
    
    * chore: delete configuration file
    
    since it is only needed for overrides

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/sunset.sh
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/sunset.sh
@@ -1,5 +1,58 @@
 #!/bin/sh
-CONTENT=$(curl -s https://freegeoip.app/json/)
-longitude=$(echo $CONTENT | jq .longitude)
-latitude=$(echo $CONTENT | jq .latitude)
-wlsunset -l $latitude -L $longitude
+
+#Startup function
+function start(){
+	[[ -f "$HOME/.config/wlsunset/config" ]] && source "$HOME/.config/wlsunset/config"
+	temp_low=${temp_low:-"4000"}
+	temp_high=${temp_high:-"6500"}
+	duration=${duration:-"900"}
+	sunrise=${sunrise:-"07:00"}
+	sunset=${sunset:-"23:00"}
+	longitude=${longitude:-65}
+	latitude=${latitude:-65}
+	location=${location:-"off"}
+
+	if [ "${location}" = "on" ]; 
+	then
+		CONTENT=$(curl -s https://freegeoip.app/json/)
+		longitude=${$(echo $CONTENT | jq '.longitude // empty'):-"${longitude}"}
+		latitude=${$(echo $CONTENT | jq '.latitude // empty'):-"${latitude}"}
+		wlsunset -l $latitude -L $longitude -t $temp_low -T $temp_high -d $duration &
+	else
+		wlsunset -t $temp_low -T $temp_high -d $duration -S $sunrise -s $sunset &
+	fi
+}
+
+#Accepts managing parameter 
+case $1'' in
+	'off')
+   	pkill wlsunset
+	;;
+
+	'on')
+	start
+	;;
+
+	'toggle')
+   	if pkill -0 wlsunset
+	then
+		pkill wlsunset
+	else
+		start
+	fi
+    ;;
+    'check')
+    command -v wlsunset
+    exit $?
+    ;;
+esac
+
+#Returns a string for Waybar 
+if pkill -0 wlsunset
+then
+	class="on"
+else
+	class="off"
+fi	
+
+printf '{"alt":"%s"}\n' "$class"
