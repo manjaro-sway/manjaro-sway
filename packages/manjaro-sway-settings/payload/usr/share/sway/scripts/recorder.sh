1251e8b9ef0fd1f6ab7963c94909701bd0262317
Jonas Strassel <info@jonas-strassel.de>
2021-08-01 16:07:31 +0200

    feat: streamline screenshot and recording modes (#37)
    * feat: reduce options and change mode before screenshot
    
    * chore: switch to printscrn for screenshot mode
    
    * chore: move definitions to.. definitions
    
    * feat: add recording script
    
    * chore: use nerd font icon for screenshot mode
    
    * feat: update recorder mode
    
    * feat: switch to print only for screenshot mode
    
    * feat: recording indicator
    
    * feat: make --gif optional
    
    * feat: allow audio and gif to be on/off
    
    * fix: remove broken bottom padding of help
    
    * feat: switch to webp
    
    * revert: go back to inverval of 5s on temp
    
    * revert: add back the percentages etc
    
    * feat: make clipboard the default screenshot sink
    
    * fix: switch back to lightbulb for sunset indicator
    
    * feat: add lock-mode
    
    * chore: remove distancing span
    
    * chore: add icon for shutdown menu
    
    * docs: align pango layout

---
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/recorder.sh
@@ -0,0 +1,47 @@
+#!/bin/bash 
+set -x
+
+pid=`pgrep wf-recorder`
+status=$?
+
+gif=false
+
+countdown() {
+  notify "Recording in 3 seconds" -t 1000
+  sleep 1
+  notify "Recording in 2 seconds" -t 1000
+  sleep 1
+  notify "Recording in 1 seconds" -t 1000
+  sleep 1
+}
+
+notify() {
+    line=$1
+    shift
+    notify-send "Recording" "${line}" -i /usr/share/icons/Papirus-Dark/32x32/devices/camera-video.svg $*;
+}
+
+if [ $status != 0 ]
+then
+    target_path=$(xdg-user-dir VIDEOS)
+    timestamp=$(date +'recording_%Y%m%d-%H%M%S')
+
+    notify "Select a region to record" -t 1000
+    area=$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp)
+
+    countdown
+    (sleep 0.5 && pkill -RTMIN+8 waybar) &
+
+    if [ "$1" == "--audio" ]; then
+        file="$target_path/$timestamp.mp4"
+        wf-recorder --audio -g "$area" --file="$file"
+    else
+        file="$target_path/$timestamp.webm"
+        wf-recorder -g "$area" -c libvpx --codec-param="qmin=0" --codec-param="qmax=25" --codec-param="crf=4" --codec-param="b:v=1M" --file="$file" 
+    fi
+
+    pkill -RTMIN+8 waybar && notify "Finished recording ${file}"
+else
+    pkill --signal SIGINT wf-recorder
+    pkill -RTMIN+8 waybar
+fi
