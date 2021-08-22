71e914e15244149c66617ebe39d2c4f697ac0862
Jonas Strassel <jo.strassel@gmail.com>
2021-08-22 10:29:24 +0200

    feat: disown waybar and foot startup scripts

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/waybar.sh
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/waybar.sh
@@ -12,4 +12,4 @@ if [ -f $USER_STYLE_PATH ]; then
     USER_STYLE=$USER_STYLE_PATH
 fi
 
-waybar -c ${USER_CONFIG:-"/usr/share/sway/templates/waybar/config.jsonc"} -s ${USER_STYLE:-"/usr/share/sway/templates/waybar/style.css"}
+waybar -c ${USER_CONFIG:-"/usr/share/sway/templates/waybar/config.jsonc"} -s ${USER_STYLE:-"/usr/share/sway/templates/waybar/style.css"} &
