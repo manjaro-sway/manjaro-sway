382fd3383d99473aa8277b086d0d52bfe854d4f7
Jonas Strassel <jo.strassel@gmail.com>
2021-08-22 02:44:13 +0200

    feat: add foot override

---
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/foot.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# wrapper script for foot
+
+USER_CONFIG_PATH=${HOME}/.config/foot/foot.ini
+
+if [ -f $USER_CONFIG_PATH ]; then
+    USER_CONFIG=$USER_CONFIG_PATH
+fi
+
+foot -c ${USER_CONFIG:-"/usr/share/sway/templates/foot.ini"}
