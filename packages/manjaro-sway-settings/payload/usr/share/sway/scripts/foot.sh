e04d66f0171219252faa60ffbf6e6395faa9240b
Andreas Böttger <206222+aboettger@users.noreply.github.com>
2022-01-30 16:32:46 +0100

    fix: use quotes around "$USER_CONFIG_PATH" (#101)

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/foot.sh
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/foot.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # wrapper script for foot
 
-USER_CONFIG_PATH=${HOME}/.config/foot/foot.ini
+USER_CONFIG_PATH="${HOME}/.config/foot/foot.ini"
 
-if [ -f $USER_CONFIG_PATH ]; then
+if [ -f "$USER_CONFIG_PATH" ]; then
     USER_CONFIG=$USER_CONFIG_PATH
 fi
 
