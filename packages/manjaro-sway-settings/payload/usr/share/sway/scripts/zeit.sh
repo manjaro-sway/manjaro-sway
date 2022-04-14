dc24d38be72ede9f9c1e82fff83626ebda422799
Jonas Strassel <info@jonas-strassel.de>
2022-04-14 10:46:05 +0200

    docs: add hint for zeit time tracker
    closes https://github.com/Manjaro-Sway/manjaro-sway/issues/269

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/zeit.sh
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/zeit.sh
@@ -4,7 +4,7 @@ tracking=$(zeit tracking --no-colors)
 if [[ "$1" == "status" ]]
 then
     text=$(echo -n $tracking | grep -q 'tracking' && echo "tracking" || echo "stopped")
-    tooltip=$tracking
+    tooltip=$tracking'\r(zeit time tracker)'
     echo {\"text\":\"$text\"\,\"tooltip\":\"$tooltip\"\,\"class\":\"$text\"\,\"alt\":\"$text\"}
 fi
 
