ec7c81720c56a8baad82ca8d5243099630a80f7e
heapifyman <274236+heapifyman@users.noreply.github.com>
2022-06-08 12:15:48 +0200

    fix: use posix compliant syntax in help.sh (#111)
    fix shellcheck warning "In POSIX sh, [[ ]] is undefined."
    [SC3010](https://github.com/koalaman/shellcheck/wiki/SC3010)

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/help.sh
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/help.sh
@@ -2,15 +2,14 @@
 set -x
 # toggles the help wrapper state
 
-OUTPUTS=$(swaymsg -t get_outputs --raw | jq -r '.[].name')
 VISIBILITY_SIGNAL=30
 QUIT_SIGNAL=31
 
-if [[ "$1" == "--toggle" ]]; then
+if [ "$1" = "--toggle" ]; then
     pkill -f -${VISIBILITY_SIGNAL} nwg-wrapper
 else
     pkill -f -${QUIT_SIGNAL} nwg-wrapper
-    for o in "$OUTPUTS"; do
-        nwg-wrapper -o $o -sv ${VISIBILITY_SIGNAL} -sq ${QUIT_SIGNAL} -s help.sh -p left -a end &
+    for output in $(swaymsg -t get_outputs --raw | jq -r '.[].name'); do
+        nwg-wrapper -o $output -sv ${VISIBILITY_SIGNAL} -sq ${QUIT_SIGNAL} -s help.sh -p left -a end &
     done
 fi
