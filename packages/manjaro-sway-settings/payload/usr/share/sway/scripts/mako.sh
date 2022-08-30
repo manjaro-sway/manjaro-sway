e32601539b6a904361850b3028c46235f0719f29
Jonas Strassel <info@jonas-strassel.de>
2022-08-30 12:27:40 +0200

    feat: add mako config file (#120)

---
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/mako.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# wrapper script for mako
+
+USER_CONFIG_PATH="${HOME}/.config/mako/config"
+
+if [ -f "$USER_CONFIG_PATH" ]; then
+    USER_CONFIG=$USER_CONFIG_PATH
+fi
+
+mako -c "${USER_CONFIG:-"/usr/share/sway/templates/mako"}" $@
