512b4d518213c4da850204a3b0d0e045317b941c
Jonas Strassel <info@jonas-strassel.de>
2022-09-10 11:50:12 +0200

    feat: add support for current bluetoothctl
    ref https://github.com/nickclyde/rofi-bluetooth/pull/21
    
    Signed-off-by: Jonas Strassel <info@jonas-strassel.de>

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/rofi-bluetooth.sh
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/rofi-bluetooth.sh
@@ -184,8 +184,14 @@ toggle_trust() {
 print_status() {
     if power_on; then
         printf ''
+        
+        paired_devices_cmd="devices Paired"
+        # Check if an outdated version of bluetoothctl is used to preserve backwards compatibility
+        if (( $(echo "$(bluetoothctl version | cut -d ' ' -f 2) < 5.65" | bc -l) )); then
+            paired_devices_cmd="paired-devices"
+        fi
 
-        mapfile -t paired_devices < <(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
+        mapfile -t paired_devices < <(bluetoothctl $paired_devices_cmd | grep Device | cut -d ' ' -f 2)
         counter=0
 
         for device in "${paired_devices[@]}"; do
