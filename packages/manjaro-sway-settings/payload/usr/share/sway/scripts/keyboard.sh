68b8ed7999685af2e403a4019e0af4925f64ad12
Jonas Strassel <info@jonas-strassel.de>
2022-10-18 00:00:35 +0200

    feat: set keyboard from localectl if 01-keyboard.conf does not exist (#132)

---
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/keyboard.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# script that sets the locale from current locale settings
+swaymsg input type:keyboard xkb_layout "$(localectl status | grep "X11 Layout" | sed -e "s/^.*X11 Layout://")"
+
+if localectl status | grep "X11 Variant" ; then
+    swaymsg input type:keyboard xkb_variant "$(localectl status | grep "X11 Variant" | sed -e "s/^.*X11 Variant://")"
+fi
