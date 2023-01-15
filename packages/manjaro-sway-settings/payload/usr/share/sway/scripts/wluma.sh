f34c46851a07de63df3ee086f189db2b10bc73a3
Jonas Strassel <info@jonas-strassel.de>
2023-01-16 00:24:43 +0100

    feat: wluma waybar module
    relates to manjaro-sway/manjaro-sway#429

---
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/wluma.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function status() {
+    systemctl --user is-active wluma >/dev/null 2>&1
+}
+
+#Accepts managing parameter
+case $1'' in
+'toggle')
+    status && systemctl --user stop wluma || systemctl --user --now enable wluma
+    ;;
+'check')
+    command -v wluma
+    exit $?
+    ;;
+esac
+
+#Returns data for Waybar
+if status; then
+    class="on"
+    text="adative brightness"
+else
+    class="off"
+    text="static brightness"
+fi
+
+printf '{"alt":"%s","text":"%s"}\n' "$class" "$text"
