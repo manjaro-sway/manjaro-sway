e141cc785477e1ad070315c764940eda24cd6289
Jonas Strassel <info@jonas-strassel.de>
2023-01-16 01:27:20 +0100

    fix: code hygiene on zeit script

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/zeit.sh
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/zeit.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
 tracking=$(zeit tracking --no-colors)
 
-if [[ "$1" == "status" ]]
-then
+case $1'' in
+'status')
     text=$(echo -n $tracking | grep -q 'tracking' && echo "tracking" || echo "stopped")
     tooltip=$tracking'\r(zeit time tracker)'
     echo {\"text\":\"$text\"\,\"tooltip\":\"$tooltip\"\,\"class\":\"$text\"\,\"alt\":\"$text\"}
-fi
-
-if [[ "$1" == "click" ]]
-then
-  if echo "$tracking" | grep -q 'tracking'
-  then
-    zeit finish
-  else
-    swaymsg exec \$zeit_list
-  fi
-fi
-
-if [[ "$1" == "track" ]]
-then
+    ;;
+'click')
+    if echo "$tracking" | grep -q 'tracking'
+    then
+        zeit finish
+    else
+        swaymsg exec \$zeit_list
+    fi
+    ;;
+'track')
     input=$(cat -)
     task=$(echo $input | pcregrep -io1 '└── (.+) \[.+')
     project=$(echo $input | pcregrep -io1 '.+\[(.+)\]')
@@ -32,4 +28,5 @@ then
 
     zeit track -p "$project" -t "$task"
     notify-send "Tracking $task in $project"
-fi
+    ;;
+esac
