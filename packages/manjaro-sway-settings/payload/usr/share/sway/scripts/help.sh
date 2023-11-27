05849eb6074dc5ca36156ad5eb266f1117d86b38
Jonas Strassel <info@jonas-strassel.de>
2023-11-27 21:03:01 +0100

    :lock: Store disabled state for help menu
    ... so it survives restarts

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/help.sh
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/help.sh
@@ -4,12 +4,28 @@ set -x
 
 VISIBILITY_SIGNAL=30
 QUIT_SIGNAL=31
+LOCKFILE="$HOME/.local/help_disabled"
 
 if [ "$1" = "--toggle" ]; then
+    if [ -f "$LOCKFILE" ]; then
+        rm "$LOCKFILE"
+    else
+        touch "$LOCKFILE"
+    fi
+    # toggles the visibility
     pkill -f -${VISIBILITY_SIGNAL} nwg-wrapper
+
 else
+    # makes sure no "old" wrappers are mounted (on start and reload)
     pkill -f -${QUIT_SIGNAL} nwg-wrapper
+    # mounts the wrapper to all outputs
     for output in $(swaymsg -t get_outputs --raw | jq -r '.[].name'); do
-        nwg-wrapper -o "$output" -sv ${VISIBILITY_SIGNAL} -sq ${QUIT_SIGNAL} -s help.sh -p left -a end &
+        # sets the initial visibility
+        if [ -f "$LOCKFILE" ]; then
+            VISIBILITY="--invisible"
+        else
+            VISIBILITY="--no-invisible"
+        fi
+        nwg-wrapper ${VISIBILITY} -o "$output" -sv ${VISIBILITY_SIGNAL} -sq ${QUIT_SIGNAL} -s help.sh -p left -a end &
     done
 fi
