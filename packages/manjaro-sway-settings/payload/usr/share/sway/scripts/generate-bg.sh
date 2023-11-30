af4877c9a47ba29d1f629319a42348b0b33ce06b
Jonas Strassel <info@jonas-strassel.de>
2023-11-30 22:58:52 +0100

    :sparkles: Background generator

---
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/generate-bg.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+set -u
+
+DEFAULT_CROWN="#00f"
+CROWN=$1
+
+DEFAULT_ROOT="#fff"
+ROOT=$2
+
+DEFAULT_BACKGROUND="#000"
+BACKGROUND=$3
+
+# shellcheck disable=SC2002
+cat /usr/share/sway/templates/manjarosway-scalable.svg | sed -e "s/${DEFAULT_CROWN}/${CROWN}/g" | sed -e "s/${DEFAULT_ROOT}/${ROOT}/g" | sed -e "s/${DEFAULT_BACKGROUND}/${BACKGROUND}/g" > "$HOME/.config/sway/generated_background.svg"
