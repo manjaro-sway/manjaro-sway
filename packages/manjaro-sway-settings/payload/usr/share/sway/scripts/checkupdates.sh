02c51a643f353a2d72dc2bbf7069cbf36f3120ce
Jonas Strassel <info@jonas-strassel.de>
2023-12-29 12:22:32 +0100

    :bug: Separate signal for pacman
    couln't update properly before and got mixed with github

---
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/checkupdates.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+case $1'' in
+'status') 
+    printf '{\"text\":\"%s\",\"tooltip\":\"%s\"}' "$(pamac checkupdates -q | wc -l)" "$(pamac checkupdates -q | awk 1 ORS='\\n' | sed 's/\\n$//')"
+    ;;
+'check')
+    [ $(pamac checkupdates -q | wc -l) -gt 0 ]
+    exit $?
+    ;;
+esac
