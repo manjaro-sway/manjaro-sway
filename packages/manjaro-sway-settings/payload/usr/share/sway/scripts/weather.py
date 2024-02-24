6884a94aab4921a98bf15d6c5d08d4569c121d25
Meow <46358138+Memoraike@users.noreply.github.com>
2024-02-24 15:35:21 +0000

    fix: missing weather widget on waybar (#188)
    * fix: missing weather widget on waybar
    
    Signed-off-by: Meow <46358138+Memoraike@users.noreply.github.com>
    
    * feat(weather): migrate to locale module
    
    Signed-off-by: Meow <46358138+Memoraike@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Meow <46358138+Memoraike@users.noreply.github.com>

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/weather.py
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/weather.py
@@ -3,6 +3,7 @@
 # credits: @bjesus https://gist.github.com/bjesus/f8db49e1434433f78e5200dc403d58a3
 
 import json
+import locale
 import os
 import requests
 import sys
@@ -83,13 +84,13 @@ WWO_CODE = {
     "395": "HeavySnowShowers",
 }
 
+# see https://docs.python.org/3/library/locale.html#background-details-hints-tips-and-caveats
+locale.setlocale(locale.LC_ALL, '')
+current_locale, _ = locale.getlocale(locale.LC_CTYPE)
 data = {}
 city = ""
 temperature = "C"
 distance = "km"
-
-if os.environ['LC_MEASUREMENT']:
-    current_locale = os.environ['LC_MEASUREMENT'].split('.')[0]
 
 if current_locale == "en_US":
     temperature = "F"
