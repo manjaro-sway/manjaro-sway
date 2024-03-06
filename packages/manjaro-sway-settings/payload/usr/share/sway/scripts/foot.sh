63d3dd86e4340dd9ab4b6683aeadc75cf0eca8e8
Jonas Strassel <info@jonas-strassel.de>
2024-03-06 23:48:58 +0100

    :alembic: Try to make footclient always use -L
    so alias etc work

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/foot.sh
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/foot.sh
@@ -7,4 +7,4 @@ if [ -f "$USER_CONFIG_PATH" ]; then
     USER_CONFIG=$USER_CONFIG_PATH
 fi
 
-foot -c "${USER_CONFIG:-"/usr/share/sway/templates/foot.ini"}" "$@" &
+foot -L -c "${USER_CONFIG:-"/usr/share/sway/templates/foot.ini"}" $@
