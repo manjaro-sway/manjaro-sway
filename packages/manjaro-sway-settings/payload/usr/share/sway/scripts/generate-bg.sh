33b9d9dc6328ebef5c23c242bdac7971261a662c
Jonas Strassel <info@jonas-strassel.de>
2024-03-07 20:28:21 +0100

    :lipstick: Also use envsubst in generate-bg

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/generate-bg.sh
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/generate-bg.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env sh
 set -xu
 
-DEFAULT_CROWN="#00f"
-CROWN=$1
-
-DEFAULT_ROOT="#fff"
-ROOT=$2
-
-DEFAULT_BACKGROUND="#000"
-BACKGROUND=$3
+export CROWN=$1
+export ROOT=$2
+export BACKGROUND=$3
 
 # shellcheck disable=SC2002
-cat /usr/share/sway/templates/manjarosway-scalable.svg | sed -e "s/${DEFAULT_CROWN}/${CROWN}/g" | sed -e "s/${DEFAULT_ROOT}/${ROOT}/g" | sed -e "s/${DEFAULT_BACKGROUND}/${BACKGROUND}/g" > "$HOME/.config/sway/generated_background.svg"
+cat /usr/share/sway/templates/manjarosway-scalable.svg | envsubst > "$HOME/.config/sway/generated_background.svg"
