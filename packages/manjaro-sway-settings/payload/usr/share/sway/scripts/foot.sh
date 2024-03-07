337080a12dfbf3649568b05b59c93dc15e324a23
Jonas Strassel <info@jonas-strassel.de>
2024-03-07 01:44:42 +0100

    :alembic: Only pass -L to the clients
    so we can has ones without login-shell still

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/foot.sh
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/foot.sh
@@ -7,4 +7,4 @@ if [ -f "$USER_CONFIG_PATH" ]; then
     USER_CONFIG=$USER_CONFIG_PATH
 fi
 
-foot -L -c "${USER_CONFIG:-"/usr/share/sway/templates/foot.ini"}" $@
+foot -c "${USER_CONFIG:-"/usr/share/sway/templates/foot.ini"}" $@
