1bca6a68473584fb7a629bc0526880672ed663be
heapifyman <274236+heapifyman@users.noreply.github.com>
2024-03-13 10:15:12 +0000

    prevent manjaro-sway-theme from overriding custom foot.ini (#190)
    * add foot-theme.ini
    
    to prevent manjaro-sway-theme from overriding custom change in a user's
    foot.ini
    
    * :sparkles: Pick the correct foot.ini (1. local, 2. theme, 3. global) automatically
    
    I think we could save the users from copying that themselves.
    
    ---------
    
    Co-authored-by: Jonas Strassel <info@jonas-strassel.de>

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/foot.sh
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/foot.sh
@@ -2,6 +2,11 @@
 # wrapper script for foot
 
 USER_CONFIG_PATH="${HOME}/.config/foot/foot.ini"
+USER_THEME_CONFIG_PATH="${HOME}/.config/foot/foot-theme.ini"
+
+if [ -f "$USER_THEME_CONFIG_PATH" ]; then
+    USER_CONFIG=$USER_THEME_CONFIG_PATH
+fi
 
 if [ -f "$USER_CONFIG_PATH" ]; then
     USER_CONFIG=$USER_CONFIG_PATH
