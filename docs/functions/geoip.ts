fb97126e3c1ad4e19b1fb26e23ce0de4bf76ecb9
Jonas Strassel <info@jonas-strassel.de>
2024-04-19 21:27:45 +0200

    :safety_vest: Add our own little ip lookup service

---
+++ docs/functions/geoip.ts
@@ -0,0 +1,35 @@
+import { Env } from "../utils";
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+    const {
+        longitude,
+        continent,
+        country,
+        isEUCountry,
+        colo,
+        timezone,
+        region,
+        regionCode,
+        latitude,
+        postalCode,
+        city
+    } = context.request.cf;
+
+    return Response.json({
+        longitude,
+        continent,
+        country,
+        isEUCountry,
+        colo,
+        timezone,
+        region,
+        regionCode,
+        latitude,
+        postalCode,
+        city
+    }, {
+        headers: {
+            "content-type": "application/json",
+        },
+    });
+};
