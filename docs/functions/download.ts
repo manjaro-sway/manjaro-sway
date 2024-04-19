8d886de04cb5cd68e6ece26fc5424eece49a2175
Jonas Strassel <info@jonas-strassel.de>
2024-04-19 23:10:57 +0200

    :bug: Count directly into a table

--- docs/functions/download.ts
+++ docs/functions/download.ts
@@ -1,15 +1,14 @@
-import { countUp, type Env } from "../utils";
+import { type Env } from "../utils";
 
 const hostname = "mirror.manjaro-sway.download";
 
 export const onRequest: PagesFunction<Env> = async (context) => {
   const url = new URL(context.request.url);
 
-
   url.hostname = hostname;
   url.pathname = `manjaro-sway/${url.searchParams.get("file")}`;
   url.searchParams.forEach((_value, name) => url.searchParams.delete(name));
-  
+
   if (context.request.method === "GET") {
     context.env.ANALYTICS_ENGINE.writeDataPoint({
       indexes: [],
@@ -18,7 +17,15 @@ export const onRequest: PagesFunction<Env> = async (context) => {
     });
 
     if (url.pathname.endsWith(".iso")) {
-        await countUp(context)
+      const currentCount = await context.env.VISITOR_COUNT_STORE.prepare(
+        "SELECT count FROM downloads WHERE timestamp = CURRENT_DATE;"
+      ).first<number>("gain");
+
+      await context.env.VISITOR_COUNT_STORE.prepare(
+		"INSERT OR REPLACE INTO downloads (count) VALUES (?);"
+      )
+        .bind((currentCount ?? 0) + 1)
+        .run();
     }
   }
 
--- docs/schema.sql
+++ docs/schema.sql
@@ -1,10 +1,7 @@
-DROP TABLE IF EXISTS visitor_statistics;
-CREATE TABLE visitor_statistics (
+DROP TABLE IF EXISTS downloads;
+CREATE TABLE downloads (
     timestamp DATETIME DEFAULT CURRENT_DATE,
     count INTEGER NOT NULL,
-    gain INTEGER NOT NULL,
     PRIMARY KEY (`timestamp`)
 );
-INSERT
-    OR REPLACE INTO visitor_statistics (timestamp, count, gain)
-VALUES ('2023-02-16', 69000, 500);
+
