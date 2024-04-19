2d857985a8c23a27b8448eabd4f2a76e09cd50c3
Jonas Strassel <info@jonas-strassel.de>
2024-04-19 22:28:34 +0200

    :sparkles: Send dowload metrics to analytics

--- docs/functions/download.ts
+++ docs/functions/download.ts
@@ -1,16 +1,26 @@
-import { countUp, Env } from "../utils";
+import { countUp, type Env } from "../utils";
 
 const hostname = "mirror.manjaro-sway.download";
 
 export const onRequest: PagesFunction<Env> = async (context) => {
-    const url = new URL(context.request.url);
-    url.hostname = hostname;
-    url.pathname = `manjaro-sway/${url.searchParams.get('file')}`
-    url.searchParams.forEach((_value, name) => url.searchParams.delete(name));
+  const url = new URL(context.request.url);
 
-    if (url.pathname.endsWith('.iso') && context.request.method === "GET") {
+
+  url.hostname = hostname;
+  url.pathname = `manjaro-sway/${url.searchParams.get("file")}`;
+  url.searchParams.forEach((_value, name) => url.searchParams.delete(name));
+  
+  if (context.request.method === "GET") {
+    context.env.ANALYTICS_ENGINE.writeDataPoint({
+      indexes: [],
+      blobs: [url.hostname, url.pathname],
+      doubles: [],
+    });
+
+    if (url.pathname.endsWith(".iso")) {
         await countUp(context)
     }
+  }
 
-    return Response.redirect(url.toString(), 301);
-}
+  return Response.redirect(url.toString(), 301);
+};
--- docs/utils.ts
+++ docs/utils.ts
@@ -1,16 +1,17 @@
 export type Env = {
-    KV: KVNamespace;
-    VISITOR_COUNT_STORE: D1Database;
-}
-type Context = EventContext<Env, any, Record<string, unknown>>
+  KV: KVNamespace;
+  VISITOR_COUNT_STORE: D1Database;
+  ANALYTICS_ENGINE: AnalyticsEngineDataset;
+};
+type Context = EventContext<Env, any, Record<string, unknown>>;
 
 export const getCount = async (context: Context) => {
-    const value = await context.env.KV.get('count');
-    return Number.parseInt(value);
-}
+  const value = await context.env.KV.get("count");
+  return Number.parseInt(value);
+};
 
 export const countUp = async (context: Context) => {
-    const current = await getCount(context);
-    await context.env.KV.put('count', (current + 1).toString());
-    return current + 1;
-}
+  const current = await getCount(context);
+  await context.env.KV.put("count", (current + 1).toString());
+  return current + 1;
+};
