3500bd3d7f6723b124ed5e0252ced6138dce4fbf
Jonas Strassel <info@jonas-strassel.de>
2024-04-21 12:50:40 +0200

    :sparkles: Best/worst day metric

--- docs/functions/count.ts
+++ docs/functions/count.ts
@@ -23,12 +23,20 @@ export const onRequest: PagesFunction<Env> = async (context) => {
 	const weekly_sum = await context.env.VISITOR_COUNT_STORE.prepare(
 		"SELECT SUM(count) as weekly_sum FROM downloads WHERE timestamp > (SELECT DATETIME('now', '-7 day'));"
 	).first<number>('weekly_sum');
+	const best_day = await context.env.VISITOR_COUNT_STORE.prepare(
+		"SELECT MAX(count) as best_day FROM downloads WHERE timestamp > '2023-02-15';"
+	).first<number>('best_day');
+	const worst_day = await context.env.VISITOR_COUNT_STORE.prepare(
+		"SELECT MIN(count) as worst_day FROM downloads WHERE timestamp > '2023-02-15';"
+	).first<number>('worst_day');
 
 	const response = new Response(JSON.stringify({
 		total: numberFormat.format(total),
 		today: numberFormat.format(today),
 		weekly_avg: numberFormat.format(Math.floor(weekly_avg)),
 		weekly_sum: numberFormat.format(Math.floor(weekly_sum)),
+		best_day: numberFormat.format(best_day),
+		worst_day: numberFormat.format(worst_day)
 	}), {
 		headers: {
 			"content-type": "application/json",
