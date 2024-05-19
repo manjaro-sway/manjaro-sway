b96e89cb5f5e60abb7d93f34934660c95f9684a0
Jonas Strassel <info@jonas-strassel.de>
2024-05-19 16:36:01 +0200

    :alembic: Does python follow redirects?

--- docs/functions/weather/index.ts
+++ docs/functions/weather/index.ts
@@ -2,6 +2,6 @@ import type { Env } from "../types";
 
 export const onRequest: PagesFunction<Env> = async (context) => {
     const url = new URL(context.request.url);
-    url.pathname = "/weather/auto";
-    return Response.redirect(url.toString(), 301);
+    url.pathname = `/weather/${context.request.cf.city}`;
+    return Response.redirect(url.toString(), 307);
 };
