c436ca75a723131d8ce8eab8f53a88e07926d0b1
Jonas Strassel <info@jonas-strassel.de>
2024-09-06 21:44:43 +0200

    cache weather requests

--- docs/functions/weather/[location].ts
+++ docs/functions/weather/[location].ts
@@ -146,6 +146,13 @@ export const onRequest: PagesFunction<Env> = async (context) => {
 
 	const weatherRequest = await fetch(
 		`https://api.open-meteo.com/v1/forecast?${searchParams.toString()}`,
+{
+					cf: {
+						// Cache the response for 
+15 minutes to not overwhelm the open-meteo rate limits
+						cacheTtl: 60 * 15,
+					},
+				},
 	);
 
 	const result = await weatherRequest.json<{
