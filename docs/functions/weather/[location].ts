b71ff434720704826ce5b8febefaa51ed292bbc5
Jonas Strassel <info@jonas-strassel.de>
2024-09-06 21:57:46 +0200

    superfluous ln

--- docs/functions/weather/[location].ts
+++ docs/functions/weather/[location].ts
@@ -148,8 +148,7 @@ export const onRequest: PagesFunction<Env> = async (context) => {
 		`https://api.open-meteo.com/v1/forecast?${searchParams.toString()}`,
 {
 					cf: {
-						// Cache the response for 
-15 minutes to not overwhelm the open-meteo rate limits
+						// Cache the response for 15 minutes to not overwhelm the open-meteo rate limits
 						cacheTtl: 60 * 15,
 					},
 				},
