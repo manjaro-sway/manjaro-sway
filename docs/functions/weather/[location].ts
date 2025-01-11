e068beaca6f33ebc1bc387ce305fcb79c9d8f881
Jonas Strassel <info@jonas-strassel.de>
2025-01-11 10:34:18 +0100

    cache open-meteo calls for half an hour
    Signed-off-by: Jonas Strassel <info@jonas-strassel.de>

--- docs/functions/weather/[location].ts
+++ docs/functions/weather/[location].ts
@@ -149,7 +149,7 @@ export const onRequest: PagesFunction<Env> = async (context) => {
 		{
 			cf: {
 				// Cache the response for 15 minutes to not overwhelm the open-meteo rate limits
-				cacheTtl: 60 * 15,
+				cacheTtl: 60 * 30,
 				cacheEverything: true,
 			},
 		},
