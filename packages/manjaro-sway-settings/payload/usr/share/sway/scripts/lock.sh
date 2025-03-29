bcfaa326ac1cde0d0a6acbbc1af4f31da5ce6ccb
ooxkf <52063318+ooxkf@users.noreply.github.com>
2025-03-30 00:03:37 +0700

    Update lock.sh to fix white background on lock-screen (#219)
    * Update lock.sh to fix white background on lock-screen
    
    After a recent update, the lock screen displayed only a white background. I found that updating the lock screen using the listed options resolved the issue.
    
    Signed-off-by: ooxkf <52063318+ooxkf@users.noreply.github.com>
    
    * fix: add special handling for swaylock-effetcs
    
    ---------
    
    Signed-off-by: ooxkf <52063318+ooxkf@users.noreply.github.com>
    Co-authored-by: Jonas Strassel <info@jonas-strassel.de>

--- packages/manjaro-sway-settings/payload/usr/share/sway/scripts/lock.sh
+++ packages/manjaro-sway-settings/payload/usr/share/sway/scripts/lock.sh
@@ -4,6 +4,8 @@ if [ -x "$(command -v gtklock)" ]; then
     gtklock --daemonize --follow-focus --idle-hide --start-hidden
 elif [ -x "$(command -v waylock)" ]; then
     waylock -fork-on-lock
-elif [ -x "$(command -v swaylock)" ]; then
+elif pacman -Qs swaylock-effects >/dev/null; then
+    swaylock --daemonize --show-failed-attempts --screenshots --clock --indicator --effect-blur 7x5 --effect-vignette 0.5:0.5 --fade-in 0.2
+elif pacman -Qs swaylock; then
     swaylock --daemonize --show-failed-attempts
 fi
